9276e4e529d47e7b79983a0018f5092fe92efca3
lingfengjgf <lingfengjgf@163.com>
2023-07-30 22:48:12 +0800

    项目模板信息配置

--- app/controller/project.js
+++ app/controller/project.js
@@ -2,10 +2,30 @@
 
 const { Controller } = require("egg");
 
+const data = [
+  {
+    id: 1,
+    name: "mini-pinia模板",
+    npmName: "lingfeng-cli",
+    url: "https://github.com/lingfengjgf/mini-pinia.git",
+    dirName: "mini-pinia",
+    version: "1.0.1",
+    download: "git",
+    type: "normal",
+    installCommand: "pnpm install",
+    startCommand: "pnpm dev",
+  },
+  {
+    id: 2,
+    name: "小程序项目模板",
+    url: "https://github.com/lingfengjgf/tomato-wallpaper.git",
+    version: "1.0.0",
+  },
+];
 class ProjectController extends Controller {
   getTemplate() {
     const { ctx } = this;
-    ctx.body = [{ a: 2 }];
+    ctx.body = data;
   }
 }
 
