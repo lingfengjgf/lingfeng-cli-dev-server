a6d27b7e6b15a7228e02593000de0cbdf5fa75af
lingfengjgf <lingfengjgf@163.com>
2023-08-01 23:14:08 +0800

    vue3模板配置

--- app/controller/project.js
+++ app/controller/project.js
@@ -5,6 +5,18 @@ const { Controller } = require("egg");
 const data = [
   {
     id: 1,
+    name: "vue3模板",
+    url: "https://github.com/lingfengjgf/template-vue3.git",
+    dirName: "template-vue3",
+    version: "1.0.0",
+    download: "git",
+    type: "normal",
+    installCommand: "pnpm install",
+    startCommand: "pnpm dev",
+    ignore: ["public/**"],
+  },
+  {
+    id: 2,
     name: "mini-pinia模板",
     npmName: "lingfeng-cli",
     url: "https://github.com/lingfengjgf/mini-pinia.git",
@@ -14,12 +26,6 @@ const data = [
     type: "normal",
     installCommand: "pnpm install",
     startCommand: "pnpm dev",
-  },
-  {
-    id: 2,
-    name: "小程序项目模板",
-    url: "https://github.com/lingfengjgf/tomato-wallpaper.git",
-    version: "1.0.0",
   },
 ];
 class ProjectController extends Controller {
